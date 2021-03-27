e1566663c4739ec634411808585f834ef7f74948
Kasra Sinaei <45814023+Kassra-sinaei@users.noreply.github.com>
2021-03-27 18:51:37 +0430

    Add files via upload
    Simulation is almost done (Motor encoder for wheel joint remaining)
    Kinematics of 4-bar Mechanism and Quaternion <-> Rotation<->Euler implmented

--- src/balance_control/src/const_pid.cpp
+++ src/balance_control/src/const_pid.cpp
@@ -1,23 +1,29 @@
 #include "robot.h"
+
 #include "ros/ros.h"
 #include "ros/console.h"
+#include<sensor_msgs/Imu.h>
+
 #include <iostream>
 #include "cmath"
 #include <math.h>
+#include <vector>
+
 #include"Eigen/Dense"
 #include "Eigen/eiquadprog.h"
 #include "Eigen/Core"
 #include "Eigen/Cholesky"
 #include "Eigen/LU"
 #include <Eigen/Geometry>
-#include <vector>
 
+using namespace Eigen;
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    ros::init(argc,argv,"home");
+    ros::init(argc,argv,"controller");
     ros::NodeHandle nh;
-    //robot robot;
+    robot wb(0.2,0.2,0.08,0.22,0.05);
 
     while(ros::ok){
         ROS_INFO("Controller Spinning");
--- src/balance_control/src/robot.h
+++ src/balance_control/src/robot.h
@@ -3,17 +3,23 @@
 
 #include "ros/ros.h"
 #include "ros/console.h"
+
 #include <iostream>
-#include "std_msgs/Float64.h"
 #include "cmath"
 #include <math.h>
+#include <vector>
+
+#include "std_msgs/Float64.h"
+#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Quaternion.h"
+#include "tf/transform_datatypes.h"
+
 #include"Eigen/Dense"
 #include "Eigen/eiquadprog.h"
 #include "Eigen/Core"
 #include "Eigen/Cholesky"
 #include "Eigen/LU"
 #include <Eigen/Geometry>
-#include <vector>
 
 using namespace Eigen;
 using namespace std;
@@ -21,12 +27,27 @@ using namespace std;
 
 class robot{
 public:
-    robot();
+    robot(double h, double s, double n, double r, double l);
+    void set_pose_attitude(Vector3d pose, VectorXd attitude);
+    //void set_params(double h, double s, double n, double r, double l);
+    double l_theta2sim(double theta1);
+    double theta2psi(double theta2);
 private:
+    double H; // Hip Length
+    double S; // Shank length
+    double N; // Knee Mechanism Lengths
+    double R; // Knee Mechanism Lengths
+    double L; // Knee Mechanism Lengths
+
     Vector3d base_pose;
     Vector3d base_attitude;
 
     float q[6];
+
+    Vector3d quaternion2euler(VectorXd quaternion);
+    Vector3d quat2rot(VectorXd quaternion);
+    Vector3d rot2rpy(MatrixXd rotation);
+    
 };
 
-#endif 
+#endif //ROBOT_H
