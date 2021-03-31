6a4354fb3a0999f14bf7301c26b89f632d59413c
Kasra Sinaei <45814023+Kassra-sinaei@users.noreply.github.com>
2021-03-31 17:59:15 +0430

    Add files via upload

--- src/robot_control/src/robot.cpp
+++ src/robot_control/src/robot.cpp
@@ -1,6 +1,6 @@
 #include "robot.h"
 
-robot::robot(double h, double s, double n, double r, double l){
+robot::robot(double h, double s, double n, double l, double r){ // Test OK
     // Constructor for defining robot's properties
     this->H = h;
     this->S = s;
@@ -9,63 +9,37 @@ robot::robot(double h, double s, double n, double r, double l){
     this->L = l;
 }
 
-Vector3d robot::rot2rpy(MatrixXd rotation){
-
-    double alpha = atan(rotation(1,0)/rotation(0,0));
-    double beta = atan(-rotation(2,0)/(sqrt(rotation(2,1)*rotation(2,1) + rotation(2,2)*rotation(2,2))));
-    double gamma = atan(rotation(2,1)/rotation(2,2));
-    Vector3d ans;
-    ans << alpha, beta, gamma;
-    return ans;
-}
-
-Vector3d robot::quat2rot(VectorXd quaternion){
-    // quaternion = [qx,qy,qz,qw]
-    double q0, q1, q2, q3;
-    q0 = quaternion(0);
-    q1 = quaternion(1);
-    q2 = quaternion(2);
-    q3 = quaternion(3);
-
-    double r00 = 2 * (q0 * q0 + q1 * q1) - 1;
-    double r01 = 2 * (q1 * q2 - q0 * q3);
-    double r02 = 2 * (q1 * q3 + q0 * q2);
-
-    double r10 = 2 * (q1 * q2 + q0 * q3);
-    double r11 = 2 * (q0 * q0 + q2 * q2) - 1;
-    double r12 = 2 * (q2 * q3 - q0 * q1);
-
-    double r20 = 2 * (q1 * q3 - q0 * q2);
-    double r21 = 2 * (q2 * q3 + q0 * q1);
-    double r22 = 2 * (q0 * q0 + q3 * q3) - 1;
-
-    MatrixXd res(3,3);
-    res << r00, r01, r02, r10, r11, r12, r20, r21, r22;
-    return res;
-
-}
-
-Vector3d robot::quaternion2euler(VectorXd quaternion){
+MatrixXd robot::quaternion2rotation(Quaterniond quaternion){
     // Calculating Euler angles from IMU Quaternion outputs
-    return(this->rot2rpy(this->quat2rot(quaternion)));
+    return(quaternion.normalized().toRotationMatrix());
 }
 
-double robot::theta2psi(double theta2){
+Vector3d robot::quaternion2ypr(Quaterniond quaternion){
+    // Calculating Euler angles from IMU Quaternion outputs
+    return quaternion.normalized().toRotationMatrix().eulerAngles(0,1,2);
+}
+
+double robot::theta2psi(double theta2){   //Test OK
     // Claculate Knee motor angle
     double a = sqrt(H*H + N*N - 2*H*N*cos(theta2));
-    double psi1 = asin(N/H * sin(theta2));
-    double psi2 = acos((a*a + R*R - L*L) / (2*L*R));
+    double psi1 = asin(N/a * sin(theta2));
+    double psi2 = acos((a*a + R*R - L*L) / (2*a*R));
+    std::cout << psi2 * 180 / M_PI << "  " << psi1 * 180 / M_PI<< std::endl;
     return psi1 + psi2;
 }
 
-double robot::l_theta2sim(double theta1){
+double robot::l_theta2sim(double theta1){   // Test OK
     // Set frame offset for left hip base joint in Gazebo
-    return theta1 + (45.0 * M_PI / 180);
+    return theta1 + (43.5 * M_PI / 180);
 }
 
-void robot::set_pose_attitude(Vector3d pose, VectorXd attitude){
+void robot::set_pose_attitude(Vector3d pose, Quaterniond attitude){
 
-    Vector3d euler = this->quaternion2euler(attitude);
+    MatrixXd euler = this->quaternion2ypr(attitude);
     this->base_pose << pose(0),pose(1),pose(2);
     this->base_attitude << euler(0), euler(1), euler(2);
+}
+
+Vector3d robot::get_attitude(){
+    return this->base_attitude;
 }
--- src/robot_control/src/robot.h
+++ src/robot_control/src/robot.h
@@ -26,28 +26,26 @@ using namespace std;
 
 
 class robot{
-public:
-    robot(double h, double s, double n, double r, double l);
-    void set_pose_attitude(Vector3d pose, VectorXd attitude);
-    //void set_params(double h, double s, double n, double r, double l);
-    double l_theta2sim(double theta1);
-    double theta2psi(double theta2);
-private:
-    double H; // Hip Length
-    double S; // Shank length
-    double N; // Knee Mechanism Lengths
-    double R; // Knee Mechanism Lengths
-    double L; // Knee Mechanism Lengths
+    public:
+        robot(double h, double s, double n, double r, double l);
+        void set_pose_attitude(Vector3d pose, Quaterniond attitude);
+        Vector3d get_attitude();
+        double l_theta2sim(double theta1);
+        double theta2psi(double theta2);
+    private:
+        double H; // Hip Length
+        double S; // Shank length
+        double N; // Knee Mechanism Lengths
+        double R; // Knee Mechanism Lengths
+        double L; // Knee Mechanism Lengths
 
-    Vector3d base_pose;
-    Vector3d base_attitude;
+        Vector3d base_pose;
+        Vector3d base_attitude;
 
-    float q[6];
+        float q[6];
 
-    Vector3d quaternion2euler(VectorXd quaternion);
-    Vector3d quat2rot(VectorXd quaternion);
-    Vector3d rot2rpy(MatrixXd rotation);
-    
+        MatrixXd quaternion2rotation(Quaterniond quaternion);
+        Vector3d quaternion2ypr(Quaterniond quaternion);
 };
 
 #endif //ROBOT_H
