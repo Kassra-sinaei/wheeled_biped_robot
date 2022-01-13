d2ff89ef8f59d565a6e34127717132d82c558fbe
kassra <kasra.sinaei@gmail.com>
2022-01-14 02:23:33 +0330

    Jumping Trajectory Generator added

---
+++ robot_control/include/robot_control/jump.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <eigen3/Eigen/Eigen>
+
+#include "iostream"
+#include "fstream"
+#include <vector>
+#include "math.h"
+#include "cmath"
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+using namespace Eigen;
+using namespace std;
+
+class Jump{
+    public:
+        Jump(double time_step);
+        double* generateTraj(double l_0, double l_d_0, double l_dd_0, double l_to, double t_0, double t_to, double t_recover, double zd);
+        ~Jump();
+    private:
+        bool fitPoly5(double (&res)[6], double init_x, double init_xd, double init_xdd,double end_x, double end_xd, double end_xdd, double t);
+        double dt;
+};
---
+++ robot_control/src/jump.cpp
@@ -0,0 +1,58 @@
+#include "jump.h"
+
+Jump::Jump(double time_step){
+    this->dt = time_step;
+}
+
+double* Jump::generateTraj(double l_0, double l_d_0, double l_dd_0, 
+                double l_to, double t_0, double t_to, double t_recover, double zd){
+    // Define Constraints on Trajectories
+    double l_dd_to = -9.81;
+    double l_td = l_to;
+    double l_dd_td = -9.81;
+    double t_0 = 0;
+    double l_d_to = sqrt(2 * 9.81 * (zd - l_to));
+    double l_d_td = -l_d_to;
+    double t_td = t_to + 2 * l_d_to / 9.81;
+    t_recover += t_td;
+
+    double* res = new double[int(t_recover / this->dt)];
+    double coefs[6];
+
+    this->fitPoly5(coefs, l_0, l_d_0, l_dd_0, l_to, l_d_to, l_dd_to, t_to - t_0);
+    int i = 0;
+    for (double time = t_0; time < t_to; time += this->dt){
+        res[i] = coefs[0] + coefs[1] * time + coefs[2] * pow(time, 2) + coefs[3] * pow(time, 3) + 
+                + coefs[4] * pow(time, 4) + coefs[5] * pow(time, 5);
+        i++;
+    }
+
+    this->fitPoly5(coefs, l_to, l_d_to, l_dd_to, l_td, l_d_td, l_dd_td, t_td-t_to);
+    for (double time = t_to; time < t_td; time += this->dt){
+        res[i] = coefs[0] + coefs[1] * time + coefs[2] * pow(time, 2) + coefs[3] * pow(time, 3) + 
+                + coefs[4] * pow(time, 4) + coefs[5] * pow(time, 5);
+        i++;
+    }
+
+    this->fitPoly5(coefs, l_td, l_d_td, l_dd_td, l_0, l_d_0, l_dd_0, t_recover - t_td);
+    for (double time = t_td; time < t_recover; time += this->dt){
+        res[i] = coefs[0] + coefs[1] * time + coefs[2] * pow(time, 2) + coefs[3] * pow(time, 3) + 
+                + coefs[4] * pow(time, 4) + coefs[5] * pow(time, 5);
+        i++;
+    }
+
+    return res;
+
+}
+
+bool Jump::fitPoly5(double (&res)[6], double init_x, double init_xd, 
+                double init_xdd,double end_x, double end_xd, double end_xdd, double t){
+    res[0] = init_x;
+    res[1] = init_xd;
+    res[2] = 0.5 * init_xdd;
+    res[3] = (end_xdd * pow(t, 2) - 3 * pow(t, 2) * init_xdd - 8 * end_xd * t - 12 * init_xd * t + 20 * end_x - 20 * init_x) / pow(t, 3) / 2;
+    res[4] = -(2 * end_xdd * pow(t, 2) - 3 * pow(t, 2) * init_xdd - 14 * end_xd * t - 16 * init_xd * t + 30 * end_x - 30 * init_x) / pow(t, 4) / 2;
+    res[5] = (end_xdd * pow(t, 2) - pow(t, 2) * init_xdd - 6 * end_xd * t - 6 * init_xd * t + 12 * end_x - 12 * init_x) / pow(t, 5) / 2;
+
+    return true;
+}
